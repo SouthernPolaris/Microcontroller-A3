92a8502844659f4bfdd09c452a9f0657d457a1fc
Asrith <asrithatluri@gmail.com>
2025-10-21 12:55:54 +1030

    adc

--- adc.c
+++ adc.c
@@ -1,17 +1,76 @@
-#include "adc.h"
+#include <platform.h>
+#include <adc.h>
 
-void adcInit() {
+//ADC power control
+//PCONP
+#define ADC_POWER_EN                   ((uint32_t)((1)<<12)) //Enable PCADC
+
+//IOCON Register
+#define IOCON_DIGITAL_MODE             ((uint32_t)(1<<7))
+#define IOCON_ADC_PIN_FUNC_1           ((uint32_t)(1))       //For ADC0[0-3]   
+
+//CR
+#define ADC_PDN                  ((uint32_t)((1)<<21)) 
+#define ADC_START                ((uint32_t)((1)<<24)) 
+#define ADC_PORT_SELECT(n)        ((uint32_t)((1)<<n))
+
+#define ADC_SAMPLING_FREQUENCY       (400000)                 //400kHz
+#define ADC_VREF                     (3.3)
+
+uint8_t GET_ADC0_Port(Pin pin){
+	
+	uint8_t ADC0_Pin_num;
+	if(GET_PORT_INDEX(pin)) {ADC0_Pin_num = GET_PIN_INDEX(pin) - 26;}
+	else {ADC0_Pin_num = GET_PIN_INDEX(pin) - 23;}
+	
+	return ADC0_Pin_num;
 
 }
 
-float adcReadVC() {
-    return 0.0f;
+void adc_init(void) {
+	
+    uint32_t temp;
+	uint32_t* ADC0_Port =GET_IOCON(P_ADC);
+	
+    LPC_SC -> PCONP |= ADC_POWER_EN; //Enable power output to ADC
+	
+	//Enable ADC 1
+	*ADC0_Port |= IOCON_ADC_PIN_FUNC_1; 
+	*ADC0_Port &= ~IOCON_DIGITAL_MODE;
+	
+	//Enable ADC 2
+	*(ADC0_Port+1) |= IOCON_ADC_PIN_FUNC_1; 
+	*(ADC0_Port+1) &= ~IOCON_DIGITAL_MODE;
+
+	//Enable ADC 0
+	*(ADC0_Port-1) |= IOCON_ADC_PIN_FUNC_1; 
+	*(ADC0_Port-1) &= ~IOCON_DIGITAL_MODE;
+
+	LPC_ADC -> CR = 0;
+	
+	//Define APB clock
+	temp = ADC_SAMPLING_FREQUENCY * 31;
+	temp = (PeripheralClock * 2 + temp) / (2 * temp) - 1;
+	LPC_ADC -> CR |=  (temp<<8);
+	
+	LPC_ADC -> CR |= ADC_PORT_SELECT(GET_ADC0_Port(P_ADC)) | ADC_PDN; // ADC pre-setting
+
 }
 
-float adcReadModulation() {
-    return 1.5f;
+int adc_read(char channel) {
+	
+	uint32_t data;
+	LPC_ADC -> CR &= (~0xFFUL);
+	LPC_ADC -> CR |= (1UL<<channel);
+	
+	LPC_ADC -> CR |= ADC_START; //Start conversion
+	
+	while( !(LPC_ADC->DR[channel] & (1UL<<31)) );//wait until the conversion completes
+	LPC_ADC -> CR &= ~ADC_START;
+	
+	data = ((LPC_ADC->DR[channel] >> 4) ) & 0xFFF;
+	return (data);
+
 }
 
-float adcReadPotentiometer() {
-    return 110.0f;
-}
+// *******************************ARM University Program Copyright © ARM Ltd 2014*************************************   
--- adc.h
+++ adc.h
@@ -1,10 +1,19 @@
+/*!
+ * \file      adc.h
+ * \brief     Internal analogue to digital converter (ADC) controller.
+ * \copyright ARM University Program &copy; ARM Ltd 2014.
+ */
 #ifndef ADC_H
 #define ADC_H
 
-void adcInit();
+/*! \brief Initializes the analogue to digital converter, and configures
+ *         the appropriate GPIO pin.
+ */
+void adc_init(void);
 
-float adcReadVC();
-float adcReadModulation();
-float adcReadPotentiometer();
+/*! \brief Reads the current value of the specified ADC.
+ *  \return Potential of the pin, relative to ground.
+ */
+int adc_read(char channel);
 
 #endif // ADC_H
