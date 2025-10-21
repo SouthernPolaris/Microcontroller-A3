bdf7a7c6e0e61a3a3402a85f0c59225ef57f4643
Asrith <asrithatluri@gmail.com>
2025-10-21 13:00:22 +1030

    all files

--- joystick.c
+++ joystick.c
@@ -1,9 +1,15 @@
 #include "joystick.h"
+#include <gpio.h>
+#include <platform.h>
 
 void initPortsForJoystick() {
-
+    gpio_set_mode(P_SW_UP, PullUp);
+    gpio_set_mode(P_SW_DN, PullUp);
+    gpio_set_mode(P_SW_LT, PullUp);
+    gpio_set_mode(P_SW_RT, PullUp);
+    gpio_set_mode(P_SW_CR, PullUp);
 }
 
-int readJoystick() {
-    return 0;
+int readJoystick(Pin pin) {
+    return !gpio_get(pin);
 }
