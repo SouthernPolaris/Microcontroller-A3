aabd2e1f612cacb94cef687b8cdaf5125c0a5441
Asrith <asrithatluri@gmail.com>
2025-10-24 13:11:44 +1030

    final commit for curr version

--- dac.c
+++ dac.c
@@ -8,10 +8,6 @@
 #define IOCON_DIGITAL_MODE             ((uint32_t)(1<<7))
 #define IOCON_DAC_ENABLE               ((uint32_t)(1<<16))
 
-//DAC control 
-//CR Register
-//Enable bias: maximum current is 700 uA and maximum frquency is 1MHz
-//Disenable bias: maximum current is 350 uA and maximum frquency is 400kHz 
 #define DAC_BIAS_EN         ((uint32_t)(1<<16))
 
 //Received value of output at DAC
@@ -30,5 +26,3 @@ void dac_set(int value) {
   LPC_DAC->CR = DAC_VALUE(value);
 	
 }
-
-// *******************************ARM University Program Copyright © ARM Ltd 2014*************************************   
