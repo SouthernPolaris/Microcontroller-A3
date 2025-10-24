5064832516bcb3ae738c1c3b039e266b2bf7a947
Asrith <asrithatluri@gmail.com>
2025-10-24 10:59:10 +1030

    update calc of frequency

--- calcFrequency.c
+++ calcFrequency.c
@@ -7,14 +7,17 @@ float calculateFrequency(float vc, float modulation, float base_freq) {
 
 		int base_actual_freq;
 
-		if (0 <= base_freq < 1) {
+		// TODO: Maybe look into direct mapping to a range of frequencies instead of if-else	
+		if (0 <= base_freq && base_freq < 1) {
 			base_actual_freq = 110;
-		} else if (1 <= base_freq < 2.0) {
+		} else if (1 <= base_freq && base_freq < 2.0) {
 			base_actual_freq = 220;
-		} else if (2 <= base_freq < 3.0) {
+		} else if (2 <= base_freq && base_freq < 3.0) {
 			base_actual_freq = 440;
-		} else if (3 <= base_freq < 3.4) {
+		} else if (3 <= base_freq && base_freq < 3.4) {
 			base_actual_freq = 880;
+		} else {
+			base_actual_freq = 440; // Default to 440Hz if out of range
 		}
 	
     power = vc + (2.0/3.0 * (modulation - 1.5));
